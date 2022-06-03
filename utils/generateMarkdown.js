7c918184ae6846e098804c6dc9f3de702d19f827
Camila Zagal <camilazags@gmail.com>
2022-06-03 18:23:40 -0500

    added markdown template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,6 +14,41 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ### Table of Contents
+  * [Description](#description)
+  * [Usage Information](#usage-info)
+  * [Installation](#installation)
+  * [Contribution Guidelines](#contribution-guidelines)
+  * [License](#license)
+  * [Questions](#questions)
+
+  ### Description 
+  ######[Back to Table of Contents](#description)
+  ${data.description} 
+
+  ## Usage Information
+  ######[Back to Table of Contents](#usage-info)
+  ${data.usage}
+
+  ## Installation
+  ######[Back to Table of Contents](#installation)
+  ${data.installing}
+
+  ## Contribution Guidelines 
+  ######[Back to Table of Contents](#contribution-guidelines)
+  ${data.guidelines}
+
+  ### License
+  ######[Back to Table of Contents](#license)
+  ${data.license}
+
+  ### Questions
+  ######[Back to Table of Contents](#questions)
+  If you have any questions about anything regarding this application, feel free to contact me via email through [here](${data.email}. Or visit the repository directly on [Github](${data.linkRepo}).
+  
+  You can visit my Github profile at [${data.username}](${data.linkProfile}).
+
+
 `;
 }
 
