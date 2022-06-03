c5206c98be0264691107f1eb553bf39b6f8311cb
Camila Zagal <camilazags@gmail.com>
2022-06-03 18:50:31 -0500

    Fixed the markdown template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
@@ -23,32 +10,35 @@ function generateMarkdown(data) {
   * [Questions](#questions)
 
   ### Description 
-  ######[Back to Table of Contents](#description)
+  [Back to Table of Contents](#description) 
+  <br>
   ${data.description} 
 
   ## Usage Information
-  ######[Back to Table of Contents](#usage-info)
+  [Back to Table of Contents](#usage-info)
+  <br>
   ${data.usage}
 
   ## Installation
-  ######[Back to Table of Contents](#installation)
+  [Back to Table of Contents](#installation)
+  <br>
   ${data.installing}
 
   ## Contribution Guidelines 
-  ######[Back to Table of Contents](#contribution-guidelines)
+  [Back to Table of Contents](#contribution-guidelines)
+  <br>
   ${data.guidelines}
 
   ### License
-  ######[Back to Table of Contents](#license)
+  [Back to Table of Contents](#license)
+  <br>
   ${data.license}
 
   ### Questions
-  ######[Back to Table of Contents](#questions)
+  [Back to Table of Contents](#questions)
   If you have any questions about anything regarding this application, feel free to contact me via email through [here](${data.email}. Or visit the repository directly on [Github](${data.linkRepo}).
-  
+  <br>
   You can visit my Github profile at [${data.username}](${data.linkProfile}).
-
-
 `;
 }
 
